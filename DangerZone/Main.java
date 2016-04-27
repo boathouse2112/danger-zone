d7aba69440037c5d327a5eeffb266470850d742f
nwalsh20 <nwalsh20@HS-422-16.fcsd.monroe.edu>
2016-04-27 13:08:45 -0400

    got running

--- DangerZone/Main.java
+++ DangerZone/Main.java
@@ -7,5 +7,13 @@
 
 public class Main
 {
-    
+    public static void main(String[] args)
+    {
+        
+        while(true)
+        {
+            System.out.println("running");
+            
+        }
+    }
 }
